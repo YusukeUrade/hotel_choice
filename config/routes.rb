887d819760367d4ea70e1e8452a2a62aeff22db0
Yuseke Urade <iga.japan.0604@gmail.com>
2023-11-14 15:34:32 +0900

    お問い合わせ機能　完了

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,11 @@ Rails.application.routes.draw do
   devise_scope :users do
     get "/users", to: redirect("/users/sign_up")
   end
+
+  resources :contacts, only: %i[new create] do
+    collection do
+      post :confirm
+      get :complete
+    end
+  end
 end
